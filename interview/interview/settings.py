444bb46c6828d8cf6709248f6d78f87ad29b8271
Wang Ruochen <wangkevin920@163.com>
2020-05-01 23:41:52 +0800

    Disable DEBUG

--- interview/interview/settings.py
+++ interview/interview/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = ''.join(chr(random.choice(range(256))) for _ in range(50))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['*']
 
